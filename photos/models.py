486c192f52daa6b1887740722436057f58119384
Tracy Ogutu <tracyogutu@gmail.com>
2019-12-16 00:14:55 +0300

    add tests

--- photos/models.py
+++ photos/models.py
@@ -9,11 +9,41 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def delete_category(cls,id):
+        category=cls.object.filter(id=id).delete()
+
+    @classmethod
+    def update_category(cls,id):
+        category=cls.object.filter(id=id).update(**request.data)
+
+    
+    def get_category(self):
+        return Category.select().where(Category.category_name==self)
+
 class Location(models.Model):
     location_name=models.CharField(max_length=30)
 
     def __str__(self):
         return self.location_name
+
+    def save_location(self):
+        self.save()
+
+    def get_location(self):
+        return Location.select().where(Location.location_name==self)
+
+    @classmethod
+    def delete_location(cls,id):
+        location=cls.object.filter(id=id).delete()
+
+    @classmethod
+    def update_location(cls,id):
+        location=cls.object.filter(id=id).update(**request.data)
+
 
 class Image(models.Model):
     photo_image=models.ImageField(upload_to='images/')
@@ -33,6 +63,14 @@ class Image(models.Model):
     def display_photos(cls):
         photos=cls.objects.all()
         return photos
+
+    @classmethod
+    def delete_photo(cls,id):
+        photo=cls.object.filter(id=id).delete()
+
+    @classmethod
+    def update_photo(cls,id):
+        photo=cls.object.filter(id=id).update(**request.data)
 
     @classmethod
     def search_by_category(cls,search_term):
